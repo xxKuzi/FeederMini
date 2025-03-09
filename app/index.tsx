f2b3a9a34570806b03135731f80b156ec81bc837
xxKuzi <jakub.kuzel7@gmail.com>
2025-03-09 11:38:30 +0100

    pheriperal page deletion

--- app/index.tsx
+++ app/index.tsx
@@ -4,21 +4,14 @@ import { requestPermissions } from "../hooks/useBLE";
 import { useState } from "react";
 import { View, Text } from "react-native";
 import styles from "../assets/styles/styles";
-import PeripheralPage from "../components/PeripheralPage";
 
 // Request BLE permissions on the first time it opens
 requestPermissions();
 
 export default function Index() {
   // Choose mode
-  const [mode, setMode] = useState<"central" | "peripheral" | "start">("start");
+
   return (
-    // TODO: Uncomment the following code to enable the mode selection
-    // <View style={styles.containerScreen}>
-    //   <Button title="Central Mode" onPress={() => setMode("central")} />
-    //   <Button title="Peripheral Mode" onPress={() => setMode("peripheral")} />
-    //   {mode == "central" ? <MainPage /> : <PeripheralPage />}
-    // </View>
     <View style={styles.containerScreen}>
       <MainPage />
     </View>
